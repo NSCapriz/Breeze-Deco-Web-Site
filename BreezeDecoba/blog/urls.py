6408f9eba775e3519debc16a7ec47779aedda1c1
Nicolás Capriz <nicocapriz@gmail.com>
2023-10-10 00:02:59 -0300

    Se crean las CBV de cada uno de los modelos de la app

--- BreezeDecoba/blog/urls.py
+++ BreezeDecoba/blog/urls.py
@@ -1,12 +1,54 @@
 from django.urls import path
 from blog import views
 from django.contrib.auth.views import LogoutView
+
+# URLS views
 urlpatterns = [
     path('', views.inicio, name="Inicio"),
-    path('registro/', views.registro, name="registro"),
-    path('login/', views.login_request, name="login"),
     path('nosotros/', views.nosotros, name="nosotros"),
     path('productos/', views.productos, name="productos"),
     path('contacto/', views.contacto, name="contacto"),
-    path("logout",LogoutView.as_view(template_name="blog/logout.html"), name="Logout",),
+]
+
+#URLS Registro,Login, Logout
+urlpatterns += [
+    path('registro/', views.registro, name="registro"),
+    path('login/', views.login_request, name="login"),
+    path("logout",LogoutView.as_view(template_name="blog/logout.html"), name="Logout"),
+]
+
+#URLS CBV-Clientes
+urlpatterns += [
+    path("clientes/list/", views.ClientListView.as_view(), name="ClientList"),
+    path("clientes/details/<int:pk>", views.ClientDetailsView.as_view(), name="ClientDetails"),
+    path("clientes/new/", views.ClientCreateView.as_view(), name="ClienteNew"),
+    path("clientes/edit/<int:pk>", views.ClientUpdateView.as_view(), name="ClienteEdit"),
+    path("clientes/delete/<int:pk>",views.ClientDeleteView.as_view(),name="ClientDelete",),
+]
+
+#URLS CBV-Productos
+urlpatterns += [
+    path("productos/list/", views.ProductListView.as_view(), name="ProductList"),
+    path("productos/details/<int:pk>", views.ProductDetailsView.as_view(), name="ProductDetails"),
+    path("productos/new/", views.ProductCreateView.as_view(), name="ProductNew"),
+    path("productos/edit/<int:pk>", views.ProductUpdateView.as_view(), name="ProductEdit"),
+    path("productos/delete/<int:pk>",views.ProductDeleteView.as_view(),name="ProductDelete",),
+]
+
+#URLS CBV-Categorias
+urlpatterns += [
+    path("categorias/list/", views.CategoryListView.as_view(), name="CategoryList"),
+    path("categorias/details/<int:pk>", views.CategoryDetailsView.as_view(), name="CategoryDetails"),
+    path("categorias/new/", views.CategoryCreateView.as_view(), name="CategoryNew"),
+    path("categorias/edit/<int:pk>", views.CategoryUpdateView.as_view(), name="CategoryEdit"),
+    path("categorias/delete/<int:pk>",views.CategoryDeleteView.as_view(),name="CategoryDelete",),
+]
+
+#URLS CBV-Contacto
+urlpatterns += [
+    path("contacto/list/", views.ContactoListView.as_view(), name="ContactoList"),
+    path("contacto/details/<int:pk>", views.ContactoDetailsView.as_view(), name="ContactoDetails"),
+    path("contacto/new/", views.ContactoCreateView.as_view(), name="ContactoNew"),
+    path("contacto/edit/<int:pk>", views.ContactoUpdateView.as_view(), name="ContactoEdit"),
+    path("contacto/delete/<int:pk>",views.ContactoDeleteView.as_view(),name="ContactoDelete",),
 ]
--- BreezeDecoba/blog/views.py
+++ BreezeDecoba/blog/views.py
@@ -9,6 +9,12 @@ from django.contrib.auth import login, logout, authenticate
 from blog.forms import UserRegisterForm
 from django.contrib.auth.decorators import login_required
 
+#CBV
+from django.views.generic import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+
 
 # Create your views here.
 def inicio(request):
@@ -61,6 +67,22 @@ def contacto(request):
         contactForm = ContactoForm()
     return render(request, "blog/contacto.html",{"contactForm": contactForm})
 
+def newsletter(request): #función a utilizar
+    if request.method == 'POST':
+
+        myForm = ClientForm(request.POST)
+        print(myForm)
+
+        if myForm.is_valid():
+            information = myForm.cleaned_data
+            client = Client(name=information['name'], lastname=information['lastname'], email=information['email'], age=information['age'])
+            client.save()
+            return render(request, "blog/index.html")
+    else:
+        myForm = ClientForm()
+    return render(request, "blog/register.html", {"myForm": myForm})
+
+# Función Login
 def login_request(request):
     if request.method == "POST":
         form = AuthenticationForm(request, data=request.POST)
@@ -81,6 +103,7 @@ def login_request(request):
 
     return render(request, "blog/login.html", {"form": form})
 
+# Función Registro
 def registro(request):
     if request.method == "POST":
         registerForm = UserRegisterForm(request.POST)
@@ -95,17 +118,107 @@ def registro(request):
 
     return render(request, "blog/register.html", {"registerForm": registerForm})
 
-def newsletter(request):
-    if request.method == 'POST':
+#CBV-Clientes
+class ClientListView(ListView):
+    model = Client
+    template_name = "blog/ClientesList.html"
 
-        myForm = ClientForm(request.POST)
-        print(myForm)
+class ClientDetailsView(DetailView):
+    model = Client
+    template_name = "blog/ClientesDetails.html"
 
-        if myForm.is_valid():
-            information = myForm.cleaned_data
-            client = Client(name=information['name'], lastname=information['lastname'], email=information['email'], age=information['age'])
-            client.save()
-            return render(request, "blog/index.html")
-    else:
-        myForm = ClientForm()
-    return render(request, "blog/register.html", {"myForm": myForm})
+class ClientCreateView(CreateView):
+    model = Client
+    template_name = "blog/ClientesCreate.html"
+    success_url = reverse_lazy("ClientList")
+    fields = ["name", "lastname", "email", "age"]
+
+class ClientUpdateView(UpdateView):
+    model = Client
+    template_name = "blog/ClientesUpdate.html"
+    success_url = reverse_lazy("ClientList")
+    fields = ["name", "lastname", "email", "age"]
+
+class ClientDeleteView(DeleteView):
+    model = Client
+    success_url = reverse_lazy("ClientList")
+    template_name = "blog/ClientesDelete.html"
+
+
+#CBV-Productos
+class ProductListView(ListView):
+    model = Product
+    template_name = "blog/ProductosList.html"
+
+class ProductDetailsView(DetailView):
+    model = Product
+    template_name = "blog/ProductosDetails.html"
+
+class ProductCreateView(CreateView):
+    model = Product
+    template_name = "blog/ProductosCreate.html"
+    success_url = reverse_lazy("ProductList")
+    fields = ["name_product", "description", "price"]
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    template_name = "blog/ProductosEdit.html"
+    success_url = reverse_lazy("ProductList")
+    fields = ["name_product", "description", "price"]
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    success_url = reverse_lazy("ProductList")
+    template_name = "blog/ProductosDelete.html"
+
+#CBV-Categorias
+class CategoryListView(ListView):
+    model = Category
+    template_name = "blog/CategoryList.html"
+
+class CategoryDetailsView(DetailView):
+    model = Category
+    template_name = "blog/CategoryDetails.html"
+
+class CategoryCreateView(CreateView):
+    model = Category
+    template_name = "blog/CategoryCreate.html"
+    success_url = reverse_lazy("CategoryList")
+    fields = ["category", "subcategory", "product"]
+
+class CategoryUpdateView(UpdateView):
+    model = Category
+    template_name = "blog/CategoryEdit.html"
+    success_url = reverse_lazy("CategoryList")
+    fields = ["category", "subcategory", "product"]
+
+class CategoryDeleteView(DeleteView):
+    model = Category
+    success_url = reverse_lazy("CategoryList")
+    template_name = "blog/CategoryDelete.html"
+    
+#CBV-Contacto
+class ContactoListView(ListView):
+    model = Contacto
+    template_name = "blog/ContactoList.html"
+
+class ContactoDetailsView(DetailView):
+    model = Contacto
+    template_name = "blog/ContactoDetails.html"
+
+class ContactoCreateView(CreateView):
+    model = Contacto
+    template_name = "blog/ContactoCreate.html"
+    success_url = reverse_lazy("ContactoList")
+    fields = ["user_name", "email", "consult"]
+
+class ContactoUpdateView(UpdateView):
+    model = Contacto
+    template_name = "blog/ContactoEdit.html"
+    success_url = reverse_lazy("ContactoList")
+    fields = ["user_name", "email", "consult"]
+
+class ContactoDeleteView(DeleteView):
+    model = Contacto
+    success_url = reverse_lazy("ContactoList")
+    template_name = "blog/ContactoDelete.html"
